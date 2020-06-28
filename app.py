2a874e90669c9728c13c8798b2e4b0bf47966a2e
Caio Teixeira <caio.xd63@hotmail.com>
2020-06-28 11:16:15 -0300

    Fix deploy

--- app.py
+++ app.py
@@ -6,16 +6,13 @@ from ext import database, cors, mail
 
 app = Flask(__name__)
 
+database.init_app(app)
+cors.init_app(app)
+mail.init_app(app)
 
-def create_app():
-    database.init_app(app)
-    cors.init_app(app)
-    mail.init_app(app)
+migrate = Migrate(app, database.db)
 
-    migrate = Migrate(app, database.db)
+from model.message import Message
+from blueprints.message import bp_message
 
-    from model.message import Message
-    from blueprints.message import bp_message
-
-    app.register_blueprint(bp_message)
-    return app
+app.register_blueprint(bp_message)
